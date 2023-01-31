2a7335c3f5750867a8417f13ab477c35f8c74e28
danielplatt <d.platt@web.de>
2023-01-31 13:57:17 +0000

    Add one-hot encoding facility

--- data/get_data.py
+++ data/get_data.py
@@ -1,8 +1,10 @@
 import numpy as np
 import os
+import pandas
+from tempfile import TemporaryFile
 
 
-def get_data(datasetname='original.npz'):
+def get_data(datasetname='original.npz', one_hot=False):
     '''
     Loads the preprocessed Kreuzer-Skarke dataset
 
@@ -10,10 +12,27 @@ def get_data(datasetname='original.npz'):
     :return: An npz file object, use as get_data()['x_proj'] and get_data()['y']
     '''
     filepath = os.path.join(os.path.dirname(os.path.abspath(__file__)), f'projections/{datasetname}')
-    return np.load(filepath)
+    data = np.load(filepath)
+    if not one_hot:
+        return data
+
+    outfile = TemporaryFile()
+    np.savez(outfile, x=data['x'], y=pandas.get_dummies(data['y']).to_numpy(), x_proj=data['x_proj'])
+    outfile.seek(0)  # Only needed here to simulate closing & reopening file
+    npzfile = np.load(outfile)
+
+    return npzfile
 
 
 if __name__ == '__main__':
-    print(f'Available indices: {get_data().files}')
-    print(f'Shape of x_proj: {get_data()["x_proj"].shape}')
-    print(f'Shape of y: {get_data()["y"].shape}')
+    print('Try loading not one-hot encoded data')
+    data = get_data()
+    print(f'Available indices: {data.files}')
+    print(f'Shape of x_proj: {data["x_proj"].shape}')
+    print(f'Shape of y: {data["y"].shape}')
+
+    print('Try loading one-hot encoded data')
+    data = get_data(one_hot=True)
+    print(f'Available indices: {data.files}')
+    print(f'Shape of x_proj: {data["x_proj"].shape}')
+    print(f'Shape of y: {data["y"].shape}')
