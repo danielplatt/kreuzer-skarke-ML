6db1f9ec69120f55f54fb6a3ace3f5916763f1b7
danielplatt <d.platt@web.de>
2023-06-21 21:50:24 +0200

    Delete old unused files

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,5 +2,5 @@ from .invariantmlp import InvariantMLP
 from .hartford import Hartford
 from .xgboost import XGboost
 from .vanilla_nn import Vanilla_nn
-from .pointnet import PointNet
+#from .pointnet import PointNet
 from .vision_transformer import VisionT
